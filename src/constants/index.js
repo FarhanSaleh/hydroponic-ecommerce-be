46eefb4670a2eed2d065b1b7bb4168f53e4d0a45
Farhan <anak.upload90@gmail.com>
2025-08-12 20:40:46 +0800

    feat: create constants

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const responseMessage = {
+  SUCCESS_FETCH: "Data berhasil diambil.",
+  SUCCESS_INSERT: "Data berhasil dimasukkan.",
+  SUCCESS_UPDATE: "Data berhasil diperbarui.",
+  SUCCESS_DELETE: "Data berhasil dihapus.",
+  ERROR_NOT_FOUND: "Data tidak ditemukan.",
+  ERROR_INVALID_INPUT: "Input yang diberikan tidak valid.",
+  ERROR_SERVER: "Terjadi kesalahan pada server.",
+  ERROR_UNAUTHORIZED: "Akses tidak sah.",
+  ERROR_CONFLICT: "Terjadi konflik dengan data yang sudah ada.",
+  ERROR_BAD_REQUEST: "Permintaan tidak valid. Silakan periksa input Anda.",
+  ERROR_FORBIDDEN: "Akses dilarang.",
+  FIELD_REQUIRED: "Semua field harus diisi.",
+};
