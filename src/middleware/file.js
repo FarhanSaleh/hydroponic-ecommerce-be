adc07a6494b86eb0cf1124f468df759100f5ccb5
Farhan <anak.upload90@gmail.com>
2025-08-15 22:49:40 +0800

    feat: create upload file middleware

---
+++ src/middleware/file.js
@@ -0,0 +1,35 @@
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const uploadDir = "uploads";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      req.user.id + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Hanya file gambar yang diizinkan!"), false);
+  }
+};
+
+export const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // Batas ukuran file 5 MB
+  fileFilter: fileFilter,
+});
